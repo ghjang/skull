6f0d3030ae6198c0d76bff252775b433c73d2c99
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-04-04 11:03:32 +0900

    made length metafunction accepts only type_list: - it's to make it not too generic.

--- include/skull/prelude/length.h
+++ include/skull/prelude/length.h
@@ -4,16 +4,25 @@
 
 #include <type_traits>
 
+#include "../base/tag.h"
+
 
 namespace skull::prelude
 {
     using skull::base::TL;
+    using skull::base::tag::no_elem_error_tag;
 
+    // NOTE: This will force the compiler to show 'no_elem_error_tag<...>' text
+    //          in the error messages when it's not a 'type_list' or it has no
+    //          elements at all.
     template <typename TypeList>
-    struct length;
+    struct length : no_elem_error_tag<TypeList>
+    {
+        // NOTE: made this have no 'type' member explicitly.
+    };
 
-    template <template <typename...> class TypeList, typename... xs>
-    struct length<TypeList<xs...>>
+    template <typename... xs>
+    struct length<TL<xs...>>
             : std::integral_constant<std::size_t, sizeof...(xs)>
     { };
 
