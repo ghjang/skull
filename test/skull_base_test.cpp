e9cb9c849732ecf720f6878debdfdea6b80abb66
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-04-04 11:08:56 +0900

    added is_same_template metafunction test case.

---
+++ test/skull_base_test.cpp
@@ -0,0 +1,13 @@
+#include "catch.hpp"
+
+#include <tuple>
+
+#include "skull/base/is_same_template.h"
+using namespace skull::base;
+
+
+TEST_CASE("is_same_template", "[base]")
+{
+    static_assert(is_same_template_v<std::tuple<int, int>, std::tuple<int, int>>);
+    static_assert(!is_same_template_v<std::tuple<int, int>, std::pair<int, int>>);
+}
