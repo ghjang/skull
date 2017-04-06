2b4fff7b36fc94a9d0fa5feb8eb647730783d62d
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-04-06 11:12:38 +0900

    added app/replace metafunction and test case.

--- include/skull/app/replace.h
+++ include/skull/app/replace.h
@@ -12,12 +12,13 @@ namespace skull::app
     using skull::base::TL;
     using skull::base::quote;
     using skull::base::invoke_v;
+    using skull::base::bind_last;
     using skull::prelude::map_t;
 
     /**
      * @tparam TypeList source type list
      * @tparam UnaryPredicate a unary predicate metafunction class
-     * @tparam T target type
+     * @tparam T new type
      */
     template <typename TypeList, typename UnaryPredicate, typename T>
     struct replace_if;
@@ -39,6 +40,23 @@ namespace skull::app
 
     template <typename TypeList, typename UnaryPredicate, typename T>
     using replace_if_t = typename replace_if<TypeList, UnaryPredicate, T>::type;
+
+    /**
+     * @tparam TypeList source type list
+     * @tparam T old type
+     * @tparam U new type
+     */
+    template <typename TypeList, typename T, typename U>
+    struct replace
+            : replace_if<
+                TypeList,
+                bind_last<quote<std::is_same>, T>,
+                U
+              >
+    { };
+
+    template <typename TypeList, typename T, typename U>
+    using replace_t = typename replace<TypeList, T, U>::type;
 } // namespace skull::app
 
 
--- test/skull_app_test.cpp
+++ test/skull_app_test.cpp
@@ -1,6 +1,7 @@
 #include "catch.hpp"
 
 #include <type_traits>
+#include <string>
 
 #include "skull/app.h"
 using namespace skull::app;
@@ -15,6 +16,20 @@ TEST_CASE("replace_if", "[app]")
                     TL<int, double, long>,
                     quote<std::is_floating_point>,
                     long long
+                >
+        >
+    );
+}
+
+TEST_CASE("replace", "[app]")
+{
+    static_assert(
+        std::is_same_v<
+                TL<int, long double, long, long double, std::string>,
+                replace_t<
+                    TL<int, double, long, double, std::string>,
+                    double,
+                    long double
                 >
         >
     );
