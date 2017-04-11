c20d4a415f5f960baf0c0652e65592052406d68f
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-04-11 12:07:50 +0900

    added a tuple implementation with vertical wrap.

---
+++ test/example/01_make_tuple.cpp
@@ -0,0 +1,72 @@
+#include "../catch.hpp"
+
+#include <iostream>
+#include "skull/prelude.h"
+using namespace skull::prelude;
+
+
+/**
+ * for debugging template
+ */
+template <typename T>
+struct TD;
+
+
+/**
+ * empty base class for class hierarchy generation
+ */
+struct empty_base
+{ };
+
+
+namespace my_tuple::take0
+{   
+    /**
+     * vertical wrap
+     */ 
+    template <typename T, typename Base>
+    struct v_wrap : Base
+    {
+        T value_;
+    };
+
+    template <typename Acc, typename T>
+    struct inherit
+            : type_is<
+                    v_wrap<T, Acc>
+              >
+    { };
+
+    template <typename T, typename Base>
+    auto & my_get(v_wrap<T, Base> & t)
+    {
+        return t.value_;
+    }
+
+    template <typename... T>
+    auto my_make_tuple(T &&... args)
+    {
+        using my_tuple_t = foldl_t<
+                                quote<inherit>,         // binary metafunction
+                                empty_base,             // left-side accumulation
+                                TL<std::decay_t<T>...>
+                           >;
+        my_tuple_t t{};
+        (void) std::initializer_list<int> {
+            (my_get<std::decay_t<T>>(t) = std::forward<T>(args), 0)...
+        };
+        return t;
+    }
+} // namespace my_tuple::take0
+
+
+TEST_CASE("my_make_tuple with vertical wrap", "[example]")
+{
+    using namespace my_tuple::take0;
+
+    auto t = my_make_tuple(10, "abc", 20.0);
+
+    REQUIRE(my_get<int>(t) == 10);
+    REQUIRE(std::strcmp(my_get<char const *>(t), "abc") == 0);
+    REQUIRE(my_get<double>(t) == 20.0);
+}
