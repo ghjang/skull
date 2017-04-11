7a64b418549ba36affd9a1431fba865dd72a1540
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-04-11 12:31:33 +0900

    added an tuple implementation with horizontal wrap.

--- test/example/01_make_tuple.cpp
+++ test/example/01_make_tuple.cpp
@@ -19,6 +19,7 @@ struct empty_base
 { };
 
 
+//==============================================================================
 namespace my_tuple::take0
 {   
     /**
@@ -63,6 +64,64 @@ namespace my_tuple::take0
 TEST_CASE("my_make_tuple with vertical wrap", "[example]")
 {
     using namespace my_tuple::take0;
+
+    auto t = my_make_tuple(10, "abc", 20.0);
+
+    REQUIRE(my_get<int>(t) == 10);
+    REQUIRE(std::strcmp(my_get<char const *>(t), "abc") == 0);
+    REQUIRE(my_get<double>(t) == 20.0);
+}
+
+
+//==============================================================================
+namespace my_tuple::take1
+{
+    template <typename T>
+    struct holder
+    {
+        T value_;
+    };
+
+    /**
+     * horizontal wrap
+     */ 
+    template <typename Holder, typename Base>
+    struct h_wrap : Holder, Base
+    { };
+
+    template <typename Acc, typename T>
+    struct inherit
+            : type_is<
+                    h_wrap<holder<T>, Acc>
+              >
+    { };
+
+    template <typename T, typename Base>
+    auto & my_get(h_wrap<holder<T>, Base> & t)
+    {
+        return static_cast<holder<T> &>(t).value_;
+    }
+
+    template <typename... T>
+    auto my_make_tuple(T &&... args)
+    {
+        using my_tuple_t = foldl_t<
+                                quote<inherit>,         // binary metafunction
+                                empty_base,             // left-side accumulation
+                                TL<std::decay_t<T>...>
+                           >;
+        my_tuple_t t{};
+        (void) std::initializer_list<int> {
+            (my_get<std::decay_t<T>>(t) = std::forward<T>(args), 0)...
+        };
+        return t;
+    }
+} // namespace my_tuple::take1
+
+
+TEST_CASE("my_make_tuple with horizontal wrap", "[example]")
+{
+    using namespace my_tuple::take1;
 
     auto t = my_make_tuple(10, "abc", 20.0);
 
