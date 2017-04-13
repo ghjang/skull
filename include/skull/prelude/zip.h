809d6eb521015b3ee27aac2bc949f922ae696428
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-04-14 01:37:54 +0900

    re-implemented zip on top of zipWith.

--- include/skull/prelude/zip.h
+++ include/skull/prelude/zip.h
@@ -3,6 +3,7 @@
 
 
 #include "../base/type_list.h"
+#include "../base/as_type_list.h"
 #include "../base/identity.h"
 #include "../base/prepend.h"
 #include "../base/unpack.h"
@@ -13,38 +14,12 @@
 namespace skull::prelude
 {
     using skull::base::TL;
+    using skull::base::as_type_list;
     using skull::base::type_is;
     using skull::base::prepend;
     using skull::base::unpack_t;
     using skull::base::from_integer_sequence_t;
     using skull::base::invoke_t;
-
-    //==========================================================================
-    template <typename xs, typename ys>
-    struct zip;
-
-    template <typename xs, typename ys>
-    using zip_t = typename zip<xs, ys>::type;
-
-    template <>
-    struct zip<TL<>, TL<>> : type_is<TL<>>
-    { };
-
-    template <typename x, typename... xs>
-    struct zip<TL<x, xs...>, TL<>> : type_is<TL<>>
-    { };
-
-    template <typename y, typename... ys>
-    struct zip<TL<>, TL<y, ys...>> : type_is<TL<>>
-    { };
-
-    template <typename x, typename... xs, typename y, typename... ys>
-    struct zip<TL<x, xs...>, TL<y, ys...>>
-            : prepend<
-                    TL<x, y>,
-                    zip_t<TL<xs...>, TL<ys...>>
-              >
-    { };
 
     //==========================================================================
     /**
@@ -77,6 +52,19 @@ namespace skull::prelude
                     zipWith_t<f, TL<xs...>, TL<ys...>>
               >
     { };
+
+    //==========================================================================
+    template <typename xs, typename ys>
+    struct zip
+            : zipWith<
+                quote<as_type_list>,
+                xs,
+                ys
+              >
+    { };
+
+    template <typename xs, typename ys>
+    using zip_t = typename zip<xs, ys>::type;
 } // namespace skull::prelude
 
 
